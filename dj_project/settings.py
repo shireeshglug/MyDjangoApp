1364c1c7d9cb2089927461996292bb017108cd72
shireeshglug <shireeshpro@yahoo.com>
2019-01-15 13:42:29 +0530

    initial commit

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -137,7 +137,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+
+STATIC_URL = '/static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')  #full path is created no matter what os we use
 MEDIA_URL = '/media/'
